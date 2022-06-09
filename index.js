18ddcaccc9c49391bb6861c5354a121146f1ebab
Christian Schmidt <schmidt.christian4@adesso.de>
2022-06-09 07:10:49 +0200

    refactored install button functionality

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
 // Code to handle install prompt on desktop
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-//addBtn.style.display = 'none';
+const addBtn = $('.add-button').css("display", "none");
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+window.addEventListener('beforeinstallprompt', e => {
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	e.preventDefault();
+	// Stash the event so it can be triggered later.
+	// Update UI to notify the user they can add to home screen
+	addBtn.css("display", "initial");
 
-  addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
+	addBtn.click(() => {
+		if (!e) return
+		// hide our user interface that shows our A2HS button
+		addBtn.css("display", "none");
+		// Show the prompt
+		e.prompt();
+		// Wait for the user to respond to the prompt
+		e.userChoice.then(choiceResult => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			e = null;
+		});
+	});
 });
 
 
 // Detects if device is on iOS 
 const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
+	const userAgent = window.navigator.userAgent.toLowerCase();
+	return /iphone|ipad|ipod/.test(userAgent);
 }
 // Detects if device is in standalone mode
 const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
 
 // Checks if should display install popup notification:
 if (isIos() && !isInStandaloneMode()) {
-  this.setState({ showInstallMessage: true });
+	this.setState({ showInstallMessage: true });
 }
